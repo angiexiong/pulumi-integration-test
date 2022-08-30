061f79d43c03b47d6eef4f42e71f0a8db736dc14
angiexiong <angie.xiong@merative.com>
2022-08-30 17:35:38 +0100

    use scripts

---
+++ .github/scripts/pre-pulumi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script is used to 
+
+
+set -ex
+
+clientId=$1
+clientSecret=$2
+tenantId=$3
+subscriptionId=$4
+
+# 1. Create virtual environment and install dependencies for pulumi command.
+python3 -m venv venv/ 
+source venv/bin/activate
+poetry export -f requirements.txt --output requirements.txt
+pip install -r requirements.txt
+
+# 2. Configure the service principle for pulumi command.
+pulumi config set azure-native:clientId "$clientId"
+pulumi config set azure-native:clientSecret "$clientSecret"
+pulumi config set azure-native:tenantId "$tenantId"
+pulumi config set azure-native:subscriptionId "$subscriptionId"
