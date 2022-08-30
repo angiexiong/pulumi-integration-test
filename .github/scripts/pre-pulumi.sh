c41d959af32507117ff942cb3dfa899282882434
angiexiong <angie.xiong@merative.com>
2022-08-30 17:39:56 +0100

    Update pre-pulumi.sh

--- .github/scripts/pre-pulumi.sh
+++ .github/scripts/pre-pulumi.sh
@@ -13,7 +13,7 @@ subscriptionId=$4
 # 1. Create virtual environment and install dependencies for pulumi command.
 python3 -m venv venv/ 
 source venv/bin/activate
-poetry export -f requirements.txt --output requirements.txt
+poetry export --without-hashes -f requirements.txt --output requirements.txt
 pip install -r requirements.txt
 
 # 2. Configure the service principle for pulumi command.
