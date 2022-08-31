9f46b6da4288c0ad5cd96a1e8847a6b6be138f3e
angiexiong <angie.xiong@merative.com>
2022-08-31 11:26:41 +0100

    Update pre-pulumi.sh

--- .github/scripts/pre-pulumi.sh
+++ .github/scripts/pre-pulumi.sh
@@ -20,6 +20,10 @@ pip install -r requirements.txt
 # Otherwise the pulumi commands will run in User mode.
 ./pulumi_login.sh -p datasci
 ./pulumi_wrapper.sh stack select ax-dev
+export clientId="$ARM_CLIENT_ID"
+export clientSecret="$ARM_CLIENT_SECRET"
+export tenantId="$ARM_TENANT_ID"
+export subscriptionId="$ARM_SUBSCRIPTION_ID"
 # ./pulumi_wrapper.sh config set azure-native:clientId "$ARM_CLIENT_ID"
 # ./pulumi_wrapper.sh config set azure-native:clientSecret "$ARM_CLIENT_SECRET" --secret
 # ./pulumi_wrapper.sh config set azure-native:tenantId "$ARM_TENANT_ID"
