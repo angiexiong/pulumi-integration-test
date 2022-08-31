89e6034af188bc9292e32d3ec0c249520671a2da
angiexiong <363068948@qq.com>
2022-08-31 11:51:18 +0100

    Simplify implementation

--- .github/scripts/pre-deploy.sh
+++ .github/scripts/pre-deploy.sh
@@ -5,10 +5,10 @@
 
 set -ex
 
-clientId=$1
-clientSecret=$2
-tenantId=$3
-subscriptionId=$4
+# clientId=$1
+# clientSecret=$2
+# tenantId=$3
+# subscriptionId=$4
 
 # 1. Create virtual environment and install dependencies for pulumi command.
 python3 -m venv venv/ 
@@ -20,7 +20,7 @@ pip install -r requirements.txt
 # Otherwise the pulumi commands will run in User mode.
 ./pulumi_login.sh -p datasci
 ./pulumi_wrapper.sh stack select ax-dev
-./pulumi_wrapper.sh config set azure-native:clientId "$clientId"
-./pulumi_wrapper.sh config set azure-native:clientSecret "$clientSecret" --secret
-./pulumi_wrapper.sh config set azure-native:tenantId "$tenantId"
-./pulumi_wrapper.sh config set azure-native:subscriptionId "$subscriptionId"
+./pulumi_wrapper.sh config set azure-native:clientId "$ARM_CLIENT_ID"
+./pulumi_wrapper.sh config set azure-native:clientSecret "$ARM_CLIENT_SECRET" --secret
+./pulumi_wrapper.sh config set azure-native:tenantId "$ARM_TENANT_ID"
+./pulumi_wrapper.sh config set azure-native:subscriptionId "$ARM_SUBSCRIPTION_ID"
