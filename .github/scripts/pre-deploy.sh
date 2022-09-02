b5d5cc4049007e796daf920fbd6f01a367953fbe
angiexiong <363068948@qq.com>
2022-09-02 11:16:15 +0100

    Update pre-deploy.sh

--- .github/scripts/pre-deploy.sh
+++ .github/scripts/pre-deploy.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-# This script is used to 
-
-
 set -ex
 
-# clientId=$1
-# clientSecret=$2
-# tenantId=$3
-# subscriptionId=$4
+poetry run pulumi login "azblob://$AIA_PROJECT_NAME"
+poetry run pulumi stack select "$AIA_PULUMI_STACK_NAME"
 
-# 1. Create virtual environment and install dependencies for pulumi command.
-python3 -m venv venv/ 
-source venv/bin/activate
-poetry export --without-hashes -f requirements.txt --output requirements.txt
-pip install -r requirements.txt
-
-# 2. Authenticate to Azure using a Service Principal.
-# Otherwise the pulumi commands will run in User mode...
-./pulumi_login.sh -p datasci
-./pulumi_wrapper.sh stack select "$pulumi_stack_name"
-./pulumi_wrapper.sh config set azure-native:clientId "$ARM_CLIENT_ID"
-./pulumi_wrapper.sh config set azure-native:clientSecret "$ARM_CLIENT_SECRET" --secret
-./pulumi_wrapper.sh config set azure-native:tenantId "$ARM_TENANT_ID"
-./pulumi_wrapper.sh config set azure-native:subscriptionId "$ARM_SUBSCRIPTION_ID"
+if [ "$GITHUB_REF_NAME" == "main" ]; then
+  echo "Update the resources on main."
+  poetry run pulumi update --yes
+else
+  echo "Preview the resources in a PR."
+  poetry run pulumi preview
+fi
